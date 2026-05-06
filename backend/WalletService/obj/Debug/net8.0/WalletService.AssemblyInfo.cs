f3ed41a37ca906aaa2bd5bb31ab201827cd9ad0b
aadigurjarr700 <aditya.gurjar_cs22@gla.ac.in>
2026-05-06 16:23:07 +0530

    [Aditya Gurjar] code refactored

--- backend/WalletService/obj/Debug/net8.0/WalletService.AssemblyInfo.cs
+++ backend/WalletService/obj/Debug/net8.0/WalletService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WalletService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e26e8a5bd45e799023400809c77764da8ad3bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b1900aae1baf890f037d6f2fc82fc789bbfa633")]
 [assembly: System.Reflection.AssemblyProductAttribute("WalletService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WalletService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
